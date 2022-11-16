14181be6054944b028382098802db6d4c1d282e5
Edgar <57589251+Edgar-MP@users.noreply.github.com>
2022-11-16 16:07:10 +0100

    a

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(".videoJuegoCarrusel").click(function(){
+   $(".videoJuegoCarrusel").removeClass("active");
+   $(this).addClass("active");
+   
+});
+
+$(function () {
+
+   $("div#btnMenu").click (function () {
+       $("div#menu").addClass ('open')
+   })
+
+   $("div#close").click (function () {
+       $("div#menu").removeClass ('open')
+   })
+
+   $("div#menu nav ul li").hover (function () {
+
+       const index = $(this).index ()
+       $("div.background-image:eq(" + index + ")").fadeIn (500);
+
+   }, function () {
+
+       const index = $(this).index ()
+       $("div.background-image:eq(" + index + ")").fadeOut (500);
+
+   })
+
+})
